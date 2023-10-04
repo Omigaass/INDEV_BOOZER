6575c95a532eb96fbccae5a042def6884871a3c1
Omigaass <miguelanperibeiro@gmail.com>
2023-10-04 00:03:54 -0300

    BOOZER-V0.7.4

--- HTML/ABRE_CARRINHO.php
+++ HTML/ABRE_CARRINHO.php
@@ -169,6 +169,52 @@
                         <button class="remove"><i class="bx bx-x"></i></button>
                         </td>
                     </tr>
+                    <tr class="tr">
+                        <td>
+                        <div class="product">
+                            <img src="https://picsum.photos/100/120" alt="" />
+                            <div class="info">
+                            <div class="name">Nome do produto</div>
+                            <div class="category">Categoria</div>
+                            </div>
+                        </div>
+                        </td>
+                        <td>R$ 120</td>
+                        <td>
+                        <div class="qty">
+                            <button><i class="bx bx-minus"></i></button>
+                            <span>2</span>
+                            <button><i class="bx bx-plus"></i></button>
+                        </div>
+                        </td>
+                        <td>R$ 240</td>
+                        <td>
+                        <button class="remove"><i class="bx bx-x"></i></button>
+                        </td>
+                    </tr>
+                    <tr class="tr">
+                        <td>
+                        <div class="product">
+                            <img src="https://picsum.photos/100/120" alt="" />
+                            <div class="info">
+                            <div class="name">Nome do produto</div>
+                            <div class="category">Categoria</div>
+                            </div>
+                        </div>
+                        </td>
+                        <td>R$ 120</td>
+                        <td>
+                        <div class="qty">
+                            <button><i class="bx bx-minus"></i></button>
+                            <span>2</span>
+                            <button><i class="bx bx-plus"></i></button>
+                        </div>
+                        </td>
+                        <td>R$ 240</td>
+                        <td>
+                        <button class="remove"><i class="bx bx-x"></i></button>
+                        </td>
+                    </tr>
                     </tbody>
                 </table>
                 </section>
